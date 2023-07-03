5db6ce85cfd865910b3e4d364855e223bc85781f
anandaaputrap <anandapratama4869@gmail.com>
2023-07-03 11:02:15 +0700

    ealahupdateteros

--- src/pages/Retention/TambahDataRetention.vue
+++ src/pages/Retention/TambahDataRetention.vue
@@ -1,50 +1,168 @@
 <template>
-    <div class="flex h-screen overflow-hidden">
-      <!-- Sidebar -->
-      <Sidebar :sidebarOpen="sidebarOpen" @close-sidebar="sidebarOpen = false" />
-      <!-- Content area -->
-      <div class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-        <!-- Site header -->
-        <Header :sidebarOpen="sidebarOpen" @toggle-sidebar="sidebarOpen = !sidebarOpen" />
-        <main>
-          <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-            <!-- Dashboard actions -->
-            <div class="sm:flex sm:justify-between sm:items-center mb-8">
-              <!-- Right: Actions -->
-              <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-  
+  <div class="flex h-screen overflow-hidden">
+    <!-- Sidebar -->
+    <Sidebar :sidebarOpen="sidebarOpen" @close-sidebar="sidebarOpen = false" />
+    <!-- Content area -->
+    <div
+      class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden"
+    >
+      <!-- Site header -->
+      <Header
+        :sidebarOpen="sidebarOpen"
+        @toggle-sidebar="sidebarOpen = !sidebarOpen"
+      />
+      <main>
+        <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+          <!-- Dashboard actions -->
+          <div class="sm:flex sm:justify-between sm:items-center mb-8">
+            <!-- Right: Actions -->
+            <div
+              class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2"
+            ></div>
+          </div>
+
+          <div
+            class="col-span-full xl:col-span-8 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700"
+          >
+            <header
+              class="px-5 py-4 border-b border-slate-100 dark:border-slate-700"
+            >
+              <h2 class="font-semibold text-slate-800 dark:text-slate-100">
+                Tambah Data Retention
+              </h2>
+            </header>
+            <div class="p-3">
+              <!-- Table -->
+              <div class="overflow-x-scroll overflow-y-scroll max-h-96">
+                <table class="table-auto w-full dark:text-slate-300">
+                  <!-- Table header -->
+                  <thead
+                    class="text-xs uppercase text-slate-400 dark:text-slate-500 bg-slate-50 dark:bg-slate-700 dark:bg-opacity-50 rounded-sm"
+                  >
+                    <tr>
+                      <th class="p-2">
+                        <div class="flex items-center">
+                          <input
+                            type="checkbox"
+                            v-model="selectAll"
+                            @change="toggleAllCheckboxes"
+                          />
+                        </div>
+                      </th>
+                      <th class="p-2">
+                        <div class="font-semibold text-left">Nama</div>
+                      </th>
+                      <th class="p-2">
+                        <div class="font-semibold text-left">Kode</div>
+                      </th>
+                      <th class="p-2">
+                        <div class="font-semibold text-left">Alamat</div>
+                      </th>
+                      <th class="p-2">
+                        <div class="font-semibold text-left">Telp</div>
+                      </th>
+                      <th class="p-2">
+                        <div class="font-semibold text-left">Segment</div>
+                      </th>
+                    </tr>
+                  </thead>
+                  <!-- Table body -->
+                  <tbody
+                    class="text-sm font-medium divide-y divide-slate-100 dark:divide-slate-700"
+                  >
+                    <!-- Row -->
+                    <tr v-for="(item, index) in items" :key="index">
+                      <td class="p-2">
+                        <div class="flex items-center">
+                          <input type="checkbox" v-model="item.checked" />
+                        </div>
+                      </td>
+                      <td class="p-2">
+                        <div class="text-left">{{ item.nota }}</div>
+                      </td>
+                      <td class="p-2">
+                        <div class="text-left">{{ item.tanggal }}</div>
+                      </td>
+                      <td class="p-2">
+                        <div class="text-left">{{ item.sales }}</div>
+                      </td>
+                      <td class="p-2">
+                        <div class="text-left">{{ item.cost }}</div>
+                      </td>
+                      <td class="p-2">
+                        <div class="text-left">{{ item.top }}</div>
+                      </td>
+                      <td class="p-2">
+                        <div class="flex items-center justify-center">
+                          <button
+                            class="btn btn-sm bg-cyan-500 hover:bg-cyan-600 text-white"
+                            @click="showDetail(index)"
+                          >
+                            Detail
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
-  
-            </div>
-  
-            <!-- Cards -->
-            <div class="grid grid-cols-12 gap-6">
-              Ini Halaman Tambah Data Retention
+              <div class="flex justify-end mt-3">
+                <button
+                  class="btn btn-sm bg-cyan-500 hover:bg-cyan-600 text-white"
+                  @click="saveData"
+                >
+                  Simpan
+                </button>
+              </div>
             </div>
           </div>
-        </main>
-      </div> 
+        </div>
+      </main>
     </div>
-  </template>
-  
-  <script>
-  import { ref } from 'vue'
-  import Sidebar from '../../partials/Sidebar.vue'
-  import Header from '../../partials/Header.vue'
-  
-  export default {
-    name: 'TambahDataRetention',
-    components: {
-      Sidebar,
-      Header,
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import Sidebar from "../../partials/Sidebar.vue";
+import Header from "../../partials/Header.vue";
+
+export default {
+  name: "TambahDataRetention",
+  components: {
+    Sidebar,
+    Header,
+  },
+  data() {
+    return {
+      sidebarOpen: false,
+      items: [
+        
+        // Add more data here if needed
+      ],
+      selectAll: false,
+      showAddForm: false,
+      showDetails: false,
+      selectedItemIndex: null,
+      tanggalAwal: "",
+      tanggalAkhir: "",
+      isSearching: false,
+    };
+  },
+  methods: {
+    showDetail(index) {
+      this.selectedItemIndex = index;
+      this.showDetails = true;
     },
-    setup() {
-  
-      const sidebarOpen = ref(false)
-  
-      return {
-        sidebarOpen,
-      }  
-    }
-  }
-  </script>
+    closeDetail() {
+      this.selectedItemIndex = null;
+      this.showDetails = false;
+    },
+    toggleAllCheckboxes() {
+      this.items.forEach((item) => {
+        item.checked = this.selectAll;
+      });
+    },
+  },
+};
+</script>
---
+++ src/partials/StarCheckbox.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex items-center">
+    <input type="checkbox" v-model="isChecked" class="hidden" />
+    <label
+      @click="toggleCheckbox"
+      :class="{ 'text-yellow-500': isChecked }"
+      class="cursor-pointer"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5 fill-current"
+        viewBox="0 0 20 20"
+      >
+        <path
+          v-if="isChecked"
+          d="M10 0l2.5 6.5H20l-5 3.5L15 17l-5-3.5L5 17l1.5-7L0 6.5h7.5L10 0z"
+        />
+        <path
+          v-else
+          d="M10 0l2.5 6.5H20l-5 3.5L15 17l-5-3.5L5 17l1.5-7L0 6.5h7.5L10 0z"
+          opacity=".4"
+        />
+      </svg>
+    </label>
+    <span class="ml-2">{{ label }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StarCheckbox",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    isChecked: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      },
+    },
+  },
+  methods: {
+    toggleCheckbox() {
+      this.isChecked = !this.isChecked;
+    },
+  },
+};
+</script>
