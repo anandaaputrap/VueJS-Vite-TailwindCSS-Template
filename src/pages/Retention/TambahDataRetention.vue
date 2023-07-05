c24365363bb9ec0c1759975d9eccea9fa55bc590
anandaaputrap <anandapratama4869@gmail.com>
2023-07-05 10:17:30 +0700

    perubahan pada inputan

--- src/pages/Retention/TambahDataRetention.vue
+++ src/pages/Retention/TambahDataRetention.vue
@@ -18,7 +18,108 @@
             <!-- Right: Actions -->
             <div
               class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2"
-            ></div>
+            >
+              <form>
+                <div class="grid gap-6 mb-6 md:grid-cols-2">
+                  <div>
+                    <label
+                      for="sales"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >Sales</label
+                    >
+                    <input
+                      type="text"
+                      id="sales"
+                      name="sales"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Sales"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      for="tipedokumen"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >Tipe Dokumen</label
+                    >
+                    <input
+                      type="text"
+                      id="tipedokumen"
+                      name="tipedokumen"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Doe"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      for="top"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >Top</label
+                    >
+                    <input
+                      type="number"
+                      id="top"
+                      name="top"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="0"
+                      min="0"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      for="toleransitop"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >Toleransi Top</label
+                    >
+                    <input
+                      type="number"
+                      id="toleransitop"
+                      name="toleransitop"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="0"
+                      min="0"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      for="tanggal"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >Tanggal</label
+                    >
+                    <input
+                      type="date"
+                      id="tanggal"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required
+                    />
+                  </div>
+                </div>
+                <div class="mb-6">
+                  <label
+                    for="keterangan"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >Keterangan</label
+                  >
+                  <textarea
+                    type="text"
+                    id="keterangan"
+                    name="keterangan"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Keterangan"
+                    required
+                  ></textarea>
+                </div>
+                <button
+                  type="submit"
+                  class="text-white bg-cyan-500 hover:bg-cyan-600 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-cyan-500 dark:hover:bg-cyan-600 dark:focus:ring-cyan-700 text-white"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
 
           <form class="w-full max-w-lg">
@@ -126,7 +227,7 @@
             </div>
             <!-- Filter -->
             <h3 class="text-2xl font-bold text-gray-900">Filter</h3>
-            <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="flex flex-wrap -mx-3 mb-">
               <br />
               <div class="w-full md:w-1/2 px-3 mt-5">
                 <label
